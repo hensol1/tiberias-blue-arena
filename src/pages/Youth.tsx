b782fee1e2d62e741447077f272ffae4b3e63be3
hensol1 <46450678+hensol1@users.noreply.github.com>
2025-12-04 22:57:11 +0200

    Update Youth.tsx

--- src/pages/Youth.tsx
+++ src/pages/Youth.tsx
@@ -1,5 +1,6 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { useIsMobile } from "@/hooks/use-mobile";
 import academyImage from "@/assets/youth/academy.jpeg";
 import newSponsors from "@/assets/sponsors/newsponsers.png";
 
